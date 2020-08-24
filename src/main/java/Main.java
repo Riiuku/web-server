0393a0599b6d82abfdca1badeab32f08876d6358
Lukasz <wegrzyniak.lukasz@gmail.com>
2020-08-24 19:44:18 +0200

    Get properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import config.ServerProperties;
+
 import java.util.Properties;
 
 public class Main {
 
     public static void main(String ...args) {
+        System.out.println(ServerProperties.get("server.port"));
     }
 }
---
+++ src/main/java/config/ServerProperties.java
@@ -0,0 +1,46 @@
+package config;
+
+import org.apache.logging.log4j.core.jmx.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ServerProperties {
+
+    private static Logger logger = LoggerFactory.getLogger(ServerProperties.class.getName());
+
+    private static Map<String, String> props = new HashMap<String, String>();
+
+    public static String get(String key) {
+        return props.get(key);
+    }
+
+    public static boolean has(String key) {
+        return props.containsKey(key);
+    }
+
+    static {
+        final File fileWithProperties = new File("src/main/resources/app.proprties");
+        try {
+            FileInputStream inputStream = new FileInputStream(fileWithProperties);
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            properties.keySet().forEach(
+                    key ->
+                            props.putIfAbsent(key.toString(), properties.getProperty(key.toString()))
+                    );
+        } catch (IOException e) {
+                logger.warn("[WebServer] File with properties not found!");
+        }
+
+
+    }
+
+
+}
