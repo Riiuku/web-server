22c0e40f694cdd5438874dafd4a7f032919b0a1c
Lukasz <wegrzyniak.lukasz@gmail.com>
2020-08-25 19:14:03 +0200

    Open server socket and wait for connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,30 @@
 import config.ServerProperties;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Properties;
+
+import static config.Contract.SERVER_PORT;
+
 
 public class Main {
 
-    public static void main(String ...args) {
-        System.out.println(ServerProperties.get("server.port"));
+
+    public static void main(String... args) {
+        try (ServerSocket socket = new ServerSocket(Integer.parseInt(ServerProperties.has(SERVER_PORT) ? ServerProperties.get(SERVER_PORT) : "8080"), 0, InetAddress.getByName(null))) {
+            while (true) {
+                connection(socket.accept());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Port assigned");
+        }
+    }
+
+    public static void connection(Socket clientSocket) throws IOException {
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
     }
 }
---
+++ src/main/java/config/Contract.java
@@ -0,0 +1,8 @@
+package config;
+
+public class Contract {
+
+    private Contract() {}
+
+    public static final String SERVER_PORT = "server.port";
+}
